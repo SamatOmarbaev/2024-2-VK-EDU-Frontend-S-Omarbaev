d5de4be1707a22273feeb1c5d9b80bb50b0c68cf
Samat <xzwqt@mail.ru>
2024-09-21 12:38:08 +0700

    добавил дополнительное условие на NaN  и тесты для проверки NaN, undefined и 0

--- convertBytesToHuman.test.js
+++ convertBytesToHuman.test.js
@@ -15,9 +15,12 @@ test('Возвращает false для неправильного типа да
   expect(convertBytesToHuman([])).toBe(false)
   expect(convertBytesToHuman({})).toBe(false)
   expect(convertBytesToHuman(null)).toBe(false)
+  expect(convertBytesToHuman(NaN)).toBe(false)
+  expect(convertBytesToHuman(undefined)).toBe(false)
 });
 
 test('Возвращает корректное значение для чисел', () => {
+  expect(convertBytesToHuman(0)).toBe('0 B')
   expect(convertBytesToHuman(5)).toBe('5 B')
   expect(convertBytesToHuman(1024)).toBe('1 KB')
   expect(convertBytesToHuman(123123123)).toBe('117.42 MB')
